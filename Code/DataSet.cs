f8b9e93feb8aa258a320422d51be7c5c96ba9d5c
Ali Özgür Dede <74831928+AliOzgurDede@users.noreply.github.com>
2021-02-15 21:57:20 +0300

    Create DataSet.cs

---
+++ Code/DataSet.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
+
+namespace StatLibrary
+{
+    /// <summary>
+    /// Primary data storing class of this library. Inherits from .NET List Collection
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class DataSet<T> : List<T>
+    {
+        /// <summary>
+        /// Pattern identification structure of DataSet
+        /// </summary>
+        public struct Pattern
+        {
+            public bool Stationary;
+            public bool Trending;
+            public bool Seasonal;
+        }
+
+        /// <summary>
+        /// Gets the minimum value of DataSet
+        /// </summary>
+        public double MinimumValue
+        {
+            get { return minimumValue; }
+        }
+        private double minimumValue
+        {
+            get { return minimumValue; }
+            set { minimumValue = Double.Parse(this.Min().ToString()); }
+        }
+
+        /// <summary>
+        /// Gets the minimum value of DataSet
+        /// </summary>
+        public double MaximumValue
+        {
+            get { return maximumValue; }
+        }
+        private double maximumValue
+        {
+            get { return maximumValue; }
+            set { maximumValue = Double.Parse(this.Max().ToString()); }
+        }
+
+        /// <summary>
+        /// Gets the range of DataSet
+        /// </summary>
+        public double Range
+        {
+            get { return range; }
+        }
+        private double range
+        {
+            get { return range; }
+            set { range = this.MaximumValue - this.MinimumValue; }
+        }
+
+    }
+}
